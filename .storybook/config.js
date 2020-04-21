2ae87580eff82ddd8bebfa8a3c92d5af0f5a0462
malikilamalik <malikilamalik@student.telkomuniversity.ac.id>
2020-04-21 22:00:24 +0700

    Ghost button

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,9 +2,7 @@ import React from "react";
 import { configure, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 
-
 function loadStories() {
-    require('../stories/HelloWorld.stories');
     require('../stories/Button.stories');
     require('../stories/Typography.stories');
 }
--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Button from '../src/components/Button';
-
+import GhostButton from '../src/components/ButtonGhost';
 storiesOf('Button', module)
     // use this for documentation
     .addParameters({
@@ -16,25 +16,9 @@ storiesOf('Button', module)
             <Button design="primary" size="big" text="Press Me!"/>
         </div>
     ))
-    .add('primary disabled', () => (
-         <div>
-            <Button design="primary" size="small" text="Press Me!" disabled/>
-            <Button design="primary" size="medium" text="Press Me!" disabled/>
-            <Button design="primary" size="big" text="Press Me!" disabled/>
-        </div>   
-    ))
-    .add('secondary default', () => (
+    .add('Ghost default', () => (
         <div>
-            <Button design="secondary" size="small" text="Press Me!"/>
-            <Button design="secondary" size="medium" text="Press Me!"/>
-            <Button design="secondary" size="big" text="Press Me!"/>
+            <GhostButton text="GHOST BUTTON"></GhostButton>
         </div>
-    ))
-    .add('secondary disabled', () => (
-         <div>
-            <Button design="secondary" size="small" text="Press Me!" disabled/>
-            <Button design="secondary" size="medium" text="Press Me!" disabled/>
-            <Button design="secondary" size="big" text="Press Me!" disabled/>
-        </div>   
     ))
 
