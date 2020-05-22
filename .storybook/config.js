74f47ebda582b7f5db6fd396ea0e0ef51e6046c7
malikilamalik <malikilamalik@student.telkomuniversity.ac.id>
2020-05-22 17:04:32 +0700

    fixing orders

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,8 +4,8 @@ import { withInfo } from '@storybook/addon-info';
 
 
 function loadStories() {
-    require('../stories/Typography.stories');
     require('../stories/HelloWorld.stories');
+    require('../stories/Typography.stories');
     require('../stories/Container.stories');
     require('../stories/ButtonText.stories');
     require('../stories/ButtonGroup.stories');
