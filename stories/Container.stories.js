33b5f88e08d58b9bf358d76ad5e198deb15f1e04
lovanto <rifkylovanto@gmail.com>
2020-06-04 11:31:58 +0700

    revision on container clear

--- stories/Container.stories.js
+++ stories/Container.stories.js
@@ -9,52 +9,35 @@ storiesOf('Container', module)
             source: true,
         },
     })
-    .add('Color Palette', () => (
-        <div className="container">
-            <Container backgroundColor="background-default"></Container>
-            <Container backgroundColor="background-gray"></Container>
-            <Container backgroundColor="background-aqua"></Container>
-            <Container backgroundColor="background-green"></Container>
-            <Container backgroundColor="background-purple"></Container>
-            <Container backgroundColor="background-orange"></Container>
-            <Container backgroundColor="background-red"></Container>
-            <Container backgroundColor="background-black"></Container>
-            <Container backgroundColor="background-white"></Container>
-            <Container backgroundColor="background-blue"></Container>
-            <Container backgroundColor="background-lavender"></Container>
-            <Container backgroundColor="background-yellow"></Container>
-            <Container backgroundColor="background-pink"></Container>
-        </div>
-    ))
     .add('Color Theme', () => (
         <div className="container">
-            <Container backgroundColor="background-primary"></Container>
-            <Container backgroundColor="background-primary-variant"></Container>
-            <Container backgroundColor="background-secondary"></Container>
-            <Container backgroundColor="background-secondary-variant"></Container>
-            <Container backgroundColor="background-success"></Container>
-            <Container backgroundColor="background-error"></Container>
-            <Container backgroundColor="background-warning"></Container>
-            <Container backgroundColor="background-info"></Container>
-            <Container backgroundColor="background-accent-purple"></Container>
-            <Container backgroundColor="background-accent-lavender"></Container>
-            <Container backgroundColor="background-accent-pink"></Container>
-            <Container backgroundColor="background-typography-black"></Container>
-            <Container backgroundColor="background-typography-white"></Container>
-            <Container backgroundColor="background-typography-border"></Container>
-            <Container backgroundColor="background-typography-disable"></Container>
+            <Container backgroundColor="primary"></Container>
+            <Container backgroundColor="primary-variant"></Container>
+            <Container backgroundColor="secondary"></Container>
+            <Container backgroundColor="secondary-variant"></Container>
+            <Container backgroundColor="success"></Container>
+            <Container backgroundColor="error"></Container>
+            <Container backgroundColor="warning"></Container>
+            <Container backgroundColor="info"></Container>
+            <Container backgroundColor="accent-purple"></Container>
+            <Container backgroundColor="accent-lavender"></Container>
+            <Container backgroundColor="accent-pink"></Container>
+            <Container backgroundColor="typography-black"></Container>
+            <Container backgroundColor="typography-white"></Container>
+            <Container backgroundColor="typography-border"></Container>
+            <Container backgroundColor="typography-disable"></Container>
         </div>
     ))
     .add('Color Gradient', () => (
         <div className="container">
-            <Container backgroundColor="background-celestial"></Container>
-            <Container backgroundColor="background-meridian"></Container>
-            <Container backgroundColor="background-by-design"></Container>
-            <Container backgroundColor="background-ultra-violet"></Container>
-            <Container backgroundColor="background-blue-ocean"></Container>
-            <Container backgroundColor="background-sunrise"></Container>
-            <Container backgroundColor="background-aubergin"></Container>
-            <Container backgroundColor="background-twitch"></Container>
-            <Container backgroundColor="background-witching-hour"></Container>
+            <Container backgroundColor="gradient-celestial"></Container>
+            <Container backgroundColor="gradient-meridian"></Container>
+            <Container backgroundColor="gradient-by-design"></Container>
+            <Container backgroundColor="gradient-ultra-violet"></Container>
+            <Container backgroundColor="gradient-blue-ocean"></Container>
+            <Container backgroundColor="gradient-sunrise"></Container>
+            <Container backgroundColor="gradient-aubergin"></Container>
+            <Container backgroundColor="gradient-twitch"></Container>
+            <Container backgroundColor="gradient-witching-hour"></Container>
         </div>
     ))
